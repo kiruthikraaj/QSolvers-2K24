78028ea24b330b88aacf179e7fe88aa5e600caab
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2024-07-18 16:54:30 +0530

    5: complete JS M4

--- eg.js
+++ eg.js
@@ -1,4 +1,28 @@
-a = 10;
-b = 2;
-c = a * b;
-console.log(~c);
+const person = {
+  name: "Antony",
+  age: 21,
+  address: {
+    city: "Chennai",
+    state: "TamilNadu",
+  },
+};
+
+for (let key in person) {
+  if (typeof person[key] === "object") {
+    for (let key1 in person[key]) {
+      console.log(key1, person[key][key1]);
+    }
+  } else {
+    console.log(key, person[key]);
+  }
+}
+
+const person2 = [{ name: "Antony" }, { age: 21 }];
+
+for (let item of person2) {
+  if (typeof item === "object") {
+    for (let key1 in item) {
+      console.log(key1[0].toUpperCase() + key1.substring(1), item[key1]);
+    }
+  }
+}
