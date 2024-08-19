6884cd33843ddbe6f2585a239a62ae6e24b7f7fb
kanishkumar-k <kanishkumar0409@gmail.com>
2024-08-19 15:27:20 +0530

    Currently Editing Milestone 2

---
+++ websocket-chat/server.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 8080 });
+
+server.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('message', (message) => {
+    console.log(`Received: ${message}`);
+
+    server.clients.forEach((client) => {
+      if (client !== socket && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  socket.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
